455b25b712d0d25573162a08ffcbafeebee144b3
freeman <iviewn@gmail.com>
2024-04-15 18:23:33 +0300

    header scroll

--- js/custom.js
+++ js/custom.js
@@ -383,6 +383,31 @@ $(scrollTop).click(function () {
 });
 /*end scrolltop*/
 
-
-
-
+/*header*/
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+$(window).scroll(function(event){
+    didScroll = true;
+});
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    if (st > lastScrollTop && st > navbarHeight){
+        $('header').removeClass('vis').addClass('unvis');
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('unvis').addClass('vis');
+        }
+    }
+    lastScrollTop = st;
+}
+/*end header*/
